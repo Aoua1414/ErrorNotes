75e4090a18e0d164917aa70bcaa5dc02b4dd7df7
attraore <taoua1986@gmail.com>
2022-08-25 17:46:59 +0000

    first commit

---
+++ src/test/java/com/error/ErrorNotes/ErrorNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.error.ErrorNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
